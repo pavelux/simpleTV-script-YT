38a5c8f39a0421394d4d83c818e2fc3c0d57cca0
Nexterr <68924751+Nexterr@users.noreply.github.com>
2020-11-14 20:45:10 +0300

    private video error message

--- YT.lua
+++ YT.lua
@@ -17,7 +17,7 @@
 -- поиск из окна "Открыть URL" (Ctrl+N), префиксы: - (видео), -- (плейлисты), --- (каналы), -+ (прямые трансляции)
 -- авторизаця: файл формата "Netscape HTTP Cookie File" - cookies.txt поместить в папку 'work' (https://addons.mozilla.org/en-US/firefox/addon/cookies-txt )
 -- показать на OSD плейлист / выбор качества: Ctrl+M
-local infoInFile = false
+local infoInFile = true
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
 		if not m_simpleTV.Control.CurrentAddress:match('^[%p%a%s]*https?://[%a%.]*youtu[%.combe]')
 			and not m_simpleTV.Control.CurrentAddress:match('^https?://[w%.]*hooktube%.com')
@@ -1980,6 +1980,15 @@ https://github.com/grafi-tt/lunaJson
 									t[i] = tab.playabilityStatus.errorScreen.playerErrorMessageRenderer.subreason.runs[i].text
 								end
 							title_err = table.concat(t)
+					elseif tab.playabilityStatus.errorScreen
+						and tab.playabilityStatus.errorScreen.playerErrorMessageRenderer
+						and tab.playabilityStatus.errorScreen.playerErrorMessageRenderer.subreason
+						and tab.playabilityStatus.errorScreen.playerErrorMessageRenderer.subreason.simpleText
+						then
+							title_err = tab.playabilityStatus.errorScreen.playerErrorMessageRenderer.subreason.simpleText
+							if tab.playabilityStatus.reason then
+								title = tab.playabilityStatus.reason
+							end
 					elseif tab.playabilityStatus.liveStreamability
 						and tab.playabilityStatus.liveStreamability.liveStreamabilityRenderer
 						and tab.playabilityStatus.liveStreamability.liveStreamabilityRenderer.offlineSlate
